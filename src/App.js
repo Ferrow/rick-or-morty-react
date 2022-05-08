f6760de57309616a3f34c7e08ac9f4de2038ef4b
Ferrow <gerhat.f@gmail.com>
2022-05-08 11:11:52 +0200

    removed if else, fixed rendering

--- src/App.js
+++ src/App.js
@@ -22,12 +22,9 @@ function App() {
     char: 50,
     enemy: 30,
   });
-  const [dead, setDead] = React.useState({ char: false, enemy: false });
-  const [playing, setPlaying] = React.useState(true);
 
   function newChar() {
-    setHealth({ char: 50, enemy: 30, charAlive: true, enemyAlive: true });
-    setDead({ char: false, enemy: false });
+    setHealth({ char: 50, enemy: 30 });
     setRandomNums((prev) => {
       return {
         randomOne: Math.floor(Math.random() * 826) + 1,
@@ -46,64 +43,19 @@ function App() {
       });
   }, [randomNums]);
 
-  /* const cardEl = charData.map((char) => {
-    return <Card name={char.name} img={char.image} key={char.id} hp={health} />;
-  });
-*/
-  /* function attack() {
-    setPlaying(true);
-    setHealth((prevHp) => {
-      if (health.enemy > 0 && health.char > 0) {
-        return {
-          ...prevHp,
-          char: health.char - (Math.floor(Math.random() * 6) + 1),
-          enemy: health.enemy - (Math.floor(Math.random() * 6) + 1),
-        };
-      } else if (health.enemy <= 0) {
-        setPlaying(false);
-        setDead({ char: false, enemy: true });
-        return { ...prevHp, enemy: 0, enemyAlive: false };
-      } else if (health.char <= 0) {
-        setPlaying(false);
-        setDead({ char: true, enemy: false });
-        return { ...prevHp, char: 0, charAlive: false };
-      }
-    });
-  }
-*/
-
   function attack() {
-    setPlaying(true);
-    if (health.enemy > 0 && health.char > 0) {
-      setHealth((prevHp) => {
-        return {
-          ...prevHp,
-          char: health.char - (Math.floor(Math.random() * 6) + 1),
-          enemy: health.enemy - (Math.floor(Math.random() * 6) + 1),
-        };
-      });
-    } else if (health.enemy <= 0) {
-      setPlaying(false);
-      setDead({ char: false, enemy: true });
-      setHealth((prevHp) => {
-        return { ...prevHp, enemy: 0 };
-      });
-    } else if (health.char <= 0) {
-      setPlaying(false);
-      setDead({ char: true, enemy: false });
-      setHealth((prevHp) => {
-        return { ...prevHp, char: 0 };
-      });
-    }
+    setHealth((prevHp) => {
+      return {
+        ...prevHp,
+        char: health.char - (Math.floor(Math.random() * 6) + 1),
+        enemy: health.enemy - (Math.floor(Math.random() * 6) + 1),
+      };
+    });
   }
 
   function newGame() {
     setHealth({ char: 50, enemy: 30 });
-    setPlaying(true);
-    setDead({ char: false, enemy: false });
   }
-
-  console.log(dead.enemy);
 
   return (
     <div className="App">
@@ -118,18 +70,16 @@ function App() {
             img={charData[0].image}
             key={charData[0].id}
             hp={health.char}
-            isAlive={!dead.char}
           />
           <Card
             name={charData[1].name}
             img={charData[1].image}
             key={charData[1].id}
             hp={health.enemy}
-            isAlive={!dead.enemy}
           />
         </div>
 
-        {playing ? (
+        {health.enemy > 0 && health.char ? (
           <button className="btn--attack" onClick={attack}>
             Attack
           </button>
